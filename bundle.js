913ab07835d32cb19c59e56bb7a0f17392255ed9
Gabriel Santos <devgtrue@gmail.com>
2020-08-30 20:39:33 -0300

    Const & Let

--- bundle.js
+++ bundle.js
@@ -6,20 +6,41 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-alert('Test');
-
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+var Matematica = /*#__PURE__*/function () {
+  function Matematica() {
+    _classCallCheck(this, Matematica);
   }
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
+  _createClass(Matematica, null, [{
+    key: "soma",
+    value: function soma(a, b) {
+      return a + b;
+    }
   }]);
 
-  return Teste;
+  return Matematica;
 }();
+
+console.log(Matematica.soma(1, 2)); // class List {
+//     constructor (){
+//     this.data = [];
+//  }
+//  add (data) {
+//      this.data.push(data);
+//      console.log(this.data);
+//  }
+// }
+// class TodoList extends List {
+//    constructor () {
+//        super();
+//        this.usuario = 'Diego';
+//    }
+//    mostraUsuario () {
+//        console.log(this.usuario);
+//    }
+// }
+// const MinhaLista = new TodoList();
+// document.getElementById('novotodo').onclick = function() {
+//     MinhaLista.add('Novo todo');
+// }
+// MinhaLista.mostraUsuario();
--- main.js
+++ main.js
@@ -1,10 +1,66 @@
-alert('Test');
-
-class Teste {
-    metodo() {
-
-    }
-    outro() {
-        
+class Matematica {
+    static soma ( a,  b) {
+        return a + b;
     }
 }
+
+console.log(Matematica.soma(1,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class List {
+//     constructor (){
+//     this.data = [];
+//  }
+//  add (data) {
+//      this.data.push(data);
+//      console.log(this.data);
+//  }
+// }
+// class TodoList extends List {
+//    constructor () {
+//        super();
+
+//        this.usuario = 'Diego';
+//    }
+//    mostraUsuario () {
+//        console.log(this.usuario);
+//    }
+// }
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = function() {
+//     MinhaLista.add('Novo todo');
+// }
+
+// MinhaLista.mostraUsuario();
