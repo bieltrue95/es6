dc27448020d32c68f7961268439453b32b232e73
Gabriel Santos <devgtrue@gmail.com>
2020-08-31 06:54:24 -0300

    Operações em Array

--- main.js
+++ main.js
@@ -1,66 +1,24 @@
-class Matematica {
-    static soma ( a,  b) {
-        return a + b;
-    }
-}
+const arr = [1, 3, 4, 5, 8, 9];
 
-console.log(Matematica.soma(1,2));
+const newArr = arr.map(function (item, index) {
+    return item + index;
+})
 
+console.log(newArr);
 
+const sum = arr.reduce(function (total, next) {
+    return total + next;
+});
 
+console.log(sum);
 
+const filter = arr.filter(function (item) {
+    return item % 2 === 0;
+});
 
+console.log(filter);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class List {
-//     constructor (){
-//     this.data = [];
-//  }
-//  add (data) {
-//      this.data.push(data);
-//      console.log(this.data);
-//  }
-// }
-// class TodoList extends List {
-//    constructor () {
-//        super();
-
-//        this.usuario = 'Diego';
-//    }
-//    mostraUsuario () {
-//        console.log(this.usuario);
-//    }
-// }
-// const MinhaLista = new TodoList();
-
-// document.getElementById('novotodo').onclick = function() {
-//     MinhaLista.add('Novo todo');
-// }
-
-// MinhaLista.mostraUsuario();
+const find = arr.find(function (item) {
+    return item === 2;
+})
+console.log(find);
