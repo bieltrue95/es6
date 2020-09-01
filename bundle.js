f2259a4b2251569fa81ec7f421576672b3f7aee8
Gabriel Santos <devgtrue@gmail.com>
2020-09-01 06:58:05 -0300

    Definindo valores padrão

--- bundle.js
+++ bundle.js
@@ -1,19 +1,10 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 9];
-var newArr = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr);
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum);
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 2;
-});
-console.log(find);
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+};
+
+console.log(soma(1));
+console.log(soma());
--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-const arr = [1, 3, 4, 5, 8, 9];
+const  soma = (a = 3, b= 6) =>a+b;
 
-const newArr = arr.map(function (item, index) {
-    return item + index;
-})
-
-console.log(newArr);
-
-const sum = arr.reduce(function (total, next) {
-    return total + next;
-});
-
-console.log(sum);
-
-const filter = arr.filter(function (item) {
-    return item % 2 === 0;
-});
-
-console.log(filter);
-
-const find = arr.find(function (item) {
-    return item === 2;
-})
-console.log(find);
+console.log(soma(1));
+console.log(soma());
